11d34051d07dcefb57e5fd6e218aa8c9f03612e5
vladbot2 <vladsidlar434@gmail.com>
2024-07-09 13:42:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+# #1
+# start = int(input("9: "))
+# end = int(input("2: "))
+
+# sum_even = 0
+# sum_odd = 0
+# sum = 0
+# count_even = 0
+# count_odd =  0
+# count = 0
+
+# for number in range(start, end + 1):
+#     if number % 2 == 0:
+#         sum_even += number
+#         count_even += 1
+#     else:
+#         sum_odd += number
+#         count_odd += 1
+#     if number % 9 == 0:
+#         sum += number
+#         count += 1
+
+#2
+# length = int(input("5: "))
+# symbol = input("#: ")
+
+# for i in range(length):
+#     print(symbol)
+
+
+#3
+# while True:
+#     number = int(input("7: "))
+#     if number > 0:
+#         print("Number is positive")
+#     elif number < 0:
+#         print("Number is negative")
+#     else:
+#         print("Number is equal to zero")
+    
+#     if number == 7:
+#         print("Good bye!")
+#         break
+
+
+#4
+# sum = 0
+# max = float('-inf')
+# min = float('inf')
+
+# while True:
+#     number = int(input("7: "))
+    
+#     if number == 7:
+#         print("Good bye!")
+#         break
+    
+#     sum += number
+#     if number > max:
+#         max = number
+#     if number < min:
+#         min = number
+
+# print(f"{sum}")
+# print(f"{max}")
+# print(f"{min}")
+
+
